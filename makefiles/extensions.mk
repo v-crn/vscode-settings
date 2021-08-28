00f3586bff51a5ac7170f4a2fa61fb9662c6ab4d
v-crn <var.cyrano@gmail.com>
2021-08-29 03:40:40 +0900

    add extensions.mk

---
+++ makefiles/extensions.mk
@@ -0,0 +1,24 @@
+EXT_DIR:=extensions
+DEFAULT_EXT:=${EXT_DIR}/extensions.txt
+INSTALLED_EXT:=${EXT_DIR}/installed_extensions.txt
+e:=""
+
+.PHONY: diff/ext
+diff/ext: list
+	git diff ${DEFAULT_EXT} ${INSTALLED_EXT}
+
+
+.PHONY: install
+install:
+	filepath=extensions/${e}/extensions.txt
+	sh shell/install_extensions.sh ${e}
+
+
+.PHONY: install/basic
+install/basic:
+	sh shell/install_basic_extensions.sh ${e}
+
+
+.PHONY: list
+list:
+	code --list-extensions > extensions/installed_extensions.txt
