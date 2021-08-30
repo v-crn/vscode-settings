71c955ac4c7fb53de736af15c4c4fa4fdc6a0161
v-crn <var.cyrano@gmail.com>
2021-08-31 02:27:58 +0900

    change make command names

--- makefiles/config.mk
+++ makefiles/config.mk
@@ -1,8 +1,8 @@
 CONFIG_FILES:=extensions.json keybindings.json settings.json
 LOCAL_CONFIG_DIR:=~/Library/Application\ Support/Code/User
 
-.PHONY: diff/cfg
-diff/cfg:
+.PHONY: diff/config
+diff/config:
 	for filename in ${CONFIG_FILES}; do \
 		echo $$filename; \
 		git diff .vscode/$$filename ${LOCAL_CONFIG_DIR}/$$filename; \
@@ -18,8 +18,8 @@ pull:
 	done
 
 
-.PHONY: up
-up:
+.PHONY: apply
+apply:
 	for filename in ${CONFIG_FILES}; do \
 		cp -pR ${LOCAL_CONFIG_DIR}/$$filename ${LOCAL_CONFIG_DIR}/$$filename.bak; \
 	done
