6dd40e519d7ccaab696ea53f846759f279745b91
v-crn <var.cyrano@gmail.com>
2021-09-02 00:38:40 +0900

    add a message in installing_fonts.sh

--- shell/install_fonts.sh
+++ shell/install_fonts.sh
@@ -1,3 +1,4 @@
+echo "Installing fonts ..."
 # curl -OL https://github.com/miiton/Cica/releases/download/v5.0.2/Cica_v5.0.2_with_emoji.zip
 curl -OL https://github.com/miiton/Cica/releases/download/v5.0.2/Cica_v5.0.2_without_emoji.zip
 unzip Cica_v5.0.2_without_emoji.zip -d tmp
