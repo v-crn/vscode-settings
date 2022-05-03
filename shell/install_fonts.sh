3b21116fb59c5f5b71e9221e96deb3cea53a2a54
koseihirano <var.cyrano@gmail.com>
2022-05-03 23:41:10 +0900

    update cica version

--- shell/install_fonts.sh
+++ shell/install_fonts.sh
@@ -1,6 +1,6 @@
 echo "Installing fonts ..."
-# curl -OL https://github.com/miiton/Cica/releases/download/v5.0.2/Cica_v5.0.2_with_emoji.zip
-curl -OL https://github.com/miiton/Cica/releases/download/v5.0.2/Cica_v5.0.2_without_emoji.zip
-unzip Cica_v5.0.2_without_emoji.zip -d tmp
+# curl -OL https://github.com/miiton/Cica/releases/download/v5.0.3/Cica_v5.0.3_with_emoji.zip
+curl -OL https://github.com/miiton/Cica/releases/download/v5.0.3/Cica_v5.0.3_without_emoji.zip
+unzip Cica_v5.0.3_without_emoji.zip -d tmp
 mv tmp/*.ttf ~/Library/Fonts/
 echo "Done."
